ec4416450e05f0c8772921a35add41c8c0b90030
Ronen Gilead-Raz <ronengi@gmail.com>
2017-05-16 04:11:59 +0300

    Add TokenStream, the tokens handler (night commit)

---
+++ TokenStream.cpp
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TokenStream.cpp
+ * Author: stimpy
+ * 
+ * Created on May 16, 2017, 3:07 AM
+ */
+
+
+#include <stdexcept>
+#include "TokenStream.hpp"
+
+
+#define PRINT '='
+#define QUIT 'x'
+#define PROMPT "enter expression:  "
+
+
+namespace arithmetic_parser {
+
+    
+    TokenStream::TokenStream()
+    : full(false), buffer(0) {}
+
+    
+    TokenStream::TokenStream(const TokenStream& orig) {
+    }
+
+    
+    TokenStream::~TokenStream() {
+    }
+
+    
+    void TokenStream::putback(Token tk) {
+        if (full) 
+            throw std::runtime_error("putback() into a full buffer");
+        buffer = tk;
+        full = true;
+    }
+
+    Token TokenStream::get() {
+        if (full) {
+            full = false;
+            return buffer;
+        }
+        char ch;
+        std::cin >> ch;
+        switch(ch) {
+            case PRINT:  case QUIT:
+            case '(':  case ')':  case '{':  case '}':
+            case '!':
+            case '+':  case '-':  case '*':  case '/':
+                return Token(ch);
+            case '.':  case '0':  case '1':  case '2':  case '3':  case '4':
+            case '5':  case '6':  case '7':  case '8':  case '9': {
+                std::cin.putback(ch);
+                double val;
+                std::cin >> val;
+                return Token('8', val);
+            }
+            default:
+                throw std::runtime_error("Bad token");
+        }
+
+    }
+    
+} // arithmetic_parser
---
+++ TokenStream.hpp
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TokenStream.hpp
+ * Author: stimpy
+ *
+ * Created on May 16, 2017, 3:07 AM
+ */
+
+#ifndef TOKENSTREAM_HPP
+#define TOKENSTREAM_HPP
+
+
+#include "Token.hpp"
+
+namespace arithmetic_parser {
+    
+    class TokenStream {
+    public:
+        TokenStream();
+        TokenStream(const TokenStream& orig);
+        virtual ~TokenStream();
+        Token get();
+        void putback(Token t);
+    private:
+        bool full;
+        Token buffer;
+    };
+
+} // arithmetic_parser
+
+
+#endif /* TOKENSTREAM_HPP */
+
